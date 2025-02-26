6c25b62ff4a5ca46e16cad5a79449cecac1c738d
leonardo <goldenmatrix2.0@gmail.com>
2025-02-26 23:53:11 +0100

    removing features

--- visializer_node.py
+++ visializer_node.py
@@ -1,14 +1,15 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from nav_msgs.msg import Odometry
-from rclpy.qos import QoSProfile, QoSReliabilityPolicy
 
 class VisualizerNode(Node):
     def __init__(self):
         super().__init__('visualizer_node')
+
         qos_profile = QoSProfile(reliability=QoSReliabilityPolicy.BEST_EFFORT, depth=10)
 
         self.subscription = self.create_subscription(
@@ -17,34 +18,30 @@ class VisualizerNode(Node):
             self.odom_callback,
             qos_profile)
 
+        # Carica il circuito dal file CSV
         self.circuit_data = pd.read_csv('vallelunga1_circuit.csv')
         self.fig, self.ax = plt.subplots()
-        self.ax.plot(self.circuit_data['x'], self.circuit_data['y'], label='circuit')
-        self.point_plot, = self.ax.plot([], [], 'ro', markersize=8, label='closest point')
-        self.external_plot, = self.ax.plot([], [], 'bo', markersize=6, label='car')
-        self.vector_plot = self.ax.quiver([], [], [], [], angles='xy', scale_units='xy', scale=1, color='g', label='yaw')
+        self.ax.plot(self.circuit_data['x'], self.circuit_data['y'], label='Circuito')
+        self.point_plot, = self.ax.plot([], [], 'ro', markersize=8, label='Posizione attuale')
+        self.external_plot, = self.ax.plot([], [], 'bo', markersize=6, label='Punto esterno')
         self.ax.legend()
         plt.ion()
         plt.show()
     
     def odom_callback(self, msg):
-        pos_x = msg.pose.pose.position.x
-        pos_y = msg.pose.pose.position.y
-        pos_z = msg.pose.pose.position.z
+        # Estrai i dati dalla posizione e orientamento
+        nn_pos_x = msg.pose.pose.position.x
+        nn_pos_y = msg.pose.pose.position.y
+        yaw = msg.pose.pose.position.z  # Usato per l'angolo
         
-        orient_x = msg.pose.pose.orientation.z
-        orient_y = msg.pose.pose.orientation.w
+        pos_x = msg.pose.pose.orientation.z  # Punto esterno x
+        pos_y = msg.pose.pose.orientation.w  # Punto esterno y
         
-        self.point_plot.set_data(pos_x, pos_y)
+        # Aggiorna il punto attuale sul circuito
+        self.point_plot.set_data(nn_pos_x, nn_pos_y)
         
-        self.external_plot.set_data(orient_x, orient_y)
-        
-        vector_length = 1.0
-        angle = pos_z
-        vec_x = vector_length * np.cos(angle)
-        vec_y = vector_length * np.sin(angle)
-        self.vector_plot.set_offsets([[orient_x, orient_y]])
-        self.vector_plot.set_UVC(vec_x, vec_y)
+        # Aggiorna il punto esterno
+        self.external_plot.set_data(pos_x, pos_y)
         
         self.ax.relim()
         self.ax.autoscale_view()
