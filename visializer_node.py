2a84e962b0924331f0bebba9afb419eee4650609
leonardo <goldenmatrix2.0@gmail.com>
2025-02-27 02:18:38 +0100

    yaw visualization added

--- visializer_node.py
+++ visializer_node.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy
+from scipy.spatial.transform import Rotation as R
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -22,8 +23,9 @@ class VisualizerNode(Node):
         self.circuit_data = pd.read_csv('vallelunga1_circuit.csv')
         self.fig, self.ax = plt.subplots()
         self.ax.plot(self.circuit_data['x'], self.circuit_data['y'], label='Circuito')
-        self.point_plot, = self.ax.plot([], [], 'ro', markersize=8, label='Posizione attuale')
-        self.external_plot, = self.ax.plot([], [], 'bo', markersize=6, label='Punto esterno')
+        self.point_plot, = self.ax.plot([], [], 'go', markersize=8, label='Posizione attuale')
+        self.external_plot, = self.ax.plot([], [], 'ro', markersize=6, label='Punto esterno')
+        self.yaw_line, = self.ax.plot([], [], 'r-', linewidth=2, label='Yaw')
         self.ax.legend()
         plt.ion()
         plt.show()
@@ -32,16 +34,26 @@ class VisualizerNode(Node):
         # Estrai i dati dalla posizione e orientamento
         nn_pos_x = msg.pose.pose.position.x
         nn_pos_y = msg.pose.pose.position.y
-        yaw = msg.pose.pose.position.z  # Usato per l'angolo
         
-        pos_x = msg.pose.pose.orientation.z  # Punto esterno x
+        pos_x = msg.pose.pose.position.z  # Punto esterno x
         pos_y = msg.pose.pose.orientation.w  # Punto esterno y
-        
+
+        r=R.from_euler('xzy', [msg.pose.pose.orientation.x,msg.pose.pose.orientation.y,msg.pose.pose.orientation.z])
+        r_matrix=r.as_matrix()
+
         # Aggiorna il punto attuale sul circuito
         self.point_plot.set_data(nn_pos_x, nn_pos_y)
         
         # Aggiorna il punto esterno
         self.external_plot.set_data(pos_x, pos_y)
+
+        # Calcola il punto finale del segmento (direzione della macchina)
+        vector_length = 100.0
+        end_x = pos_x + vector_length * r_matrix[2][0]
+        end_y = pos_y + vector_length * r_matrix[0][0]
+
+        # Aggiorna la linea
+        self.yaw_line.set_data([pos_x, end_x], [pos_y, end_y])
         
         self.ax.relim()
         self.ax.autoscale_view()
