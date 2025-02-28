8b6739b6819ba62ac9557910c39b6fd31fb1608c
leonardo <goldenmatrix2.0@gmail.com>
2025-02-28 14:08:23 +0100

    debby line is now here

--- visializer_node.py
+++ visializer_node.py
@@ -25,6 +25,7 @@ class VisualizerNode(Node):
         self.point_plot, = self.ax.plot([], [], 'go', markersize=8, label='Posizione attuale')
         self.external_plot, = self.ax.plot([], [], 'ro', markersize=6, label='Punto esterno')
         self.yaw_line, = self.ax.plot([], [], 'r-', linewidth=2, label='Yaw')
+        self.debby_line, = self.ax.plot([], [], 'b-', linewidth=2, label='debby')
         self.ax.legend()
         plt.ion()
         plt.show()
@@ -46,11 +47,19 @@ class VisualizerNode(Node):
         vector_length = 100.0
         end_x = pos_x + vector_length * r_matrix[0][2]
         end_y = pos_y + vector_length * r_matrix[2][2]
+
+        self.yaw_line.set_data([pos_x, end_x], [pos_y, end_y])
+
+        debby_length = 1.0
+        debby_x = nn_pos_x + debby_length * msg.pose.pose.position.z
+        debby_y = nn_pos_y + debby_length * msg.twist.twist.linear.z
+
+        self.debby_line.set_data([nn_pos_x, debby_x], [nn_pos_y, debby_y])
+
         
         theta = np.arctan2(r_matrix[2,2], r_matrix[0,2])
         print(f'x: {pos_x}, y: {pos_y}, yaw: {theta}')
 
-        self.yaw_line.set_data([pos_x, end_x], [pos_y, end_y])
         
         self.ax.relim()
         self.ax.autoscale_view()
